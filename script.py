cff7f52b392f8c48c24c09269bab8e20f647873d
admouse <arthurmateu@gmail.com>
2024-11-15 18:07:18 +0100

    script

---
+++ script.py
@@ -0,0 +1,35 @@
+import os
+import re
+import argparse
+
+# create args
+parser = argparse.ArgumentParser(prog="filetoplink", description="Gives pixiv link given a pixiv filename")
+parser.add_argument(
+    "-O", "--output",
+    default="output.txt",
+    help=".txt output filename (default: 'output.txt')")
+parser.add_argument(
+    "directory",
+    help="Directory to analyze")
+args = parser.parse_args()
+
+
+# create variables
+directory = args.directory
+output_file = args.output if args.output[-4:] == '.txt' else args.output + '.txt'
+links = []
+
+for _, _, files in os.walk(directory):
+    for file in files:
+        # if "_p<num>." in file. creates a group for the code
+        match = re.search(r'(\d+)_p\d+\.', file)
+        if match:
+            code = match.group(1)
+            link = "https://www.pixiv.net/en/artworks/" + code
+            links.append(link)
+
+
+# make output file
+with open(output_file, 'w') as file:
+    for link in links:
+        file.write(link + "\n")
